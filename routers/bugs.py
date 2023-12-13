3f35f953b051051099c279f6abdc6e57bed534d5
Alamin Shaikh <hello@octib.com>
2023-12-13 16:06:43 +0600

    Bugs endpoints drafted

---
+++ routers/bugs.py
@@ -0,0 +1,32 @@
+from fastapi import APIRouter, Request
+
+router = APIRouter()
+
+
+# Get a projects bugs
+@router.get('/projects/{project_id}/bugs')
+async def get_bugs(request: Request):
+    return {'message': 'Get bugs'}
+
+# Get a specific bug
+@router.get('/projects/{project_id}/bugs/{bug_id}')
+async def get_bugs(request: Request):
+    return {'message': 'Get a bug'}
+
+
+# Create a new bug
+@router.post('/projects/{project_id}/bugs')
+async def get_bugs(request: Request):
+    return {'message': 'Create bug'}
+
+
+# Update a bug
+@router.put('/projects/{project_id}/bugs/{bug_id}')
+async def get_bugs(request: Request):
+    return {'message': 'Update bug'}
+
+
+# Delete a bug
+@router.delete('/projects/{project_id}/bugs/{bug_id}')
+async def get_bugs(request: Request):
+    return {'message': 'Delete bug'}
