5c076dbab6ece146fc3476def9bf4c0804738878
Alamin Shaikh <hello@octib.com>
2023-12-18 14:02:04 +0600

    Create bug route fine-tuned

--- routers/bugs.py
+++ routers/bugs.py
@@ -24,6 +24,7 @@ async def get_bugs(project_id, bug_id):
 async def create_bug(bug: BugCreate, project_id: str):
     # Convert bug data to dict
     bug_dict = dict(bug)
+    bug_dict['_id'] = ObjectId()
     bug_dict['reported_by'] = dict(bug_dict['reported_by'])
     bug_dict['assigned_to'] = dict(bug_dict['assigned_to'])
 
@@ -33,13 +34,18 @@ async def create_bug(bug: BugCreate, project_id: str):
         {'_id': ObjectId(project_id)}, {'$push': {'bugs': bug_dict}}
     )
 
-    # Return the updated project
+    # Get the updated project
     updated_project = db.projects.find_one({'_id': ObjectId(project_id)})
+
+    # Update the project and bug ids
     updated_project['id'] = str(updated_project.pop('_id'))
+    updated_project['bugs'] = [{ 'id': str(bug.pop('_id')), **bug } for bug in updated_project['bugs']]
+
+    # Return the updated project
     return updated_project
 
 
 # Update a bug
 @router.put('/projects/{project_id}/bugs/{bug_id}')
-async def get_bugs(project_id, bug_id):
+async def get_bugs(project_id: str, bug_id: str, bug: BugUpdate):
     return {'message': 'Update bug'}
